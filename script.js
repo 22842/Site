cba338297aea154834e467828f2821479856326a
22842 <ostaptuhaj@gmail.com>
2026-01-13 13:14:43 +0200

    today's second page html css

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("привіт зі світу ігор")
+const images = document.querySelectorAll('.gallery img');
+const modal = document.getElementById('modal');
+const modalImg = document.getElementById('modalImg');
+const close = document.querySelector('.close');
+
+images.forEach(img => {
+    img.addEventListener('click', () => {
+        modal.style.display = 'flex';
+        modalImg.src = img.src;
+    });
+});
+
+close.addEventListener('click', () => {
+    modal.style.display = 'none';
+});
+
+modal.addEventListener('click', (e) => {
+    if (e.target === modal) {
+        modal.style.display = 'none';
+    }
+});
